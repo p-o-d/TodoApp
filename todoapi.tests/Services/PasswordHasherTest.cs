d9b40726c263c4d7cba9d83548f3ff241abff018
lazy_frog <pcuzer@gmail.com>
2019-08-03 17:29:54 +0300

    Feature: Added PasswordHasher service

--- todoapi.tests/Services/PasswordHasherTest.cs
+++ todoapi.tests/Services/PasswordHasherTest.cs
@@ -11,7 +11,7 @@ namespace todoapi.tests.Services
         [Fact]
         public void Should_Create_Hash()
         {
-        //Given
+        //Given      
             var options = Options.Create(new HashOptions{ HashIterations = 20 });
             var hashInput = "testpass";
             var hasher = new PasswordHasher(options);
@@ -38,6 +38,61 @@ namespace todoapi.tests.Services
             var hasher = new PasswordHasher(options);
         //When
             Assert.Throws<ArgumentNullException>(() => hasher.Hash(hashInput));
+        }
+
+        [Fact]
+        public void Should_Verify_Valid_Hash_Without_NeedUpdate()
+        {
+        //Given
+            var options = Options.Create(new HashOptions{ HashIterations = 20 });
+            var hash = @"20.4gbglLwieS4BxkH/OvW+gw==.B+HHKuYUkj9mXmhyVHsfDxlsQXGpRdceL+5wy3HK3/k=";
+            var password = "testpass";
+            var hasher = new PasswordHasher(options);
+        //When
+            var checkResult = hasher.Check(hash, password);
+        //Then
+            Assert.True(checkResult.verified);
+            Assert.False(checkResult.needUpdate);
+        }
+
+        [Fact]
+        public void Should_Return_NeedUpdate_On_Mismatch_Iterations()
+        {
+        //Given
+            var options = Options.Create(new HashOptions{ HashIterations = 50 });
+            var hash = @"20.4gbglLwieS4BxkH/OvW+gw==.B+HHKuYUkj9mXmhyVHsfDxlsQXGpRdceL+5wy3HK3/k=";
+            var password = "testpass";
+            var hasher = new PasswordHasher(options);
+        //When
+            var checkResult = hasher.Check(hash, password);
+        //Then
+            Assert.True(checkResult.needUpdate);
+        }
+
+        [Fact]
+        public void Should_Not_Verify_Invalid_Hash()
+        {
+        //Given
+            var options = Options.Create(new HashOptions{ HashIterations = 20 });
+            var hash = @"20.4gbglLwieS4BxkH/OvW+gw==.B+HHKuYUkj9mXmhyVHsfDxlsQXGpRdceL+5wy3HK3/k=";
+            var password = "invalidpass";
+            var hasher = new PasswordHasher(options);
+        //When
+            var checkResult = hasher.Check(hash, password);
+        //Then
+            Assert.False(checkResult.verified);
+        }
+
+        [Fact]
+        public void Should_Throw_On_Invalid_Hash_Format()
+        {
+        //Given
+            var options = Options.Create(new HashOptions{ HashIterations = 20 });
+            var hash = "invalidhash";
+            var password = "invalidpass";
+            var hasher = new PasswordHasher(options);
+        //Then
+            Assert.Throws<FormatException>(() => hasher.Check(hash, password));
         }
     }
 }
--- todoapi/Services/Impl/PasswordHasher.cs
+++ todoapi/Services/Impl/PasswordHasher.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Security.Cryptography;
 using Microsoft.Extensions.Options;
 using todoapi.AppOptions;
@@ -18,7 +19,28 @@ namespace todoapi.Services.Impl
         }
         public (bool verified, bool needUpdate) Check(string hash, string password)
         {
-            throw new System.NotImplementedException();
+            if(string.IsNullOrEmpty(hash) || string.IsNullOrEmpty(password))
+                throw new ArgumentNullException();
+
+            var parts = hash.Split('.', StringSplitOptions.RemoveEmptyEntries);
+
+            if(parts.Length != 3)
+                throw new FormatException("Given hash has wrong format!");
+
+            var iterations = Int32.Parse(parts[0]);
+            var salt = Convert.FromBase64String(parts[1]);
+            var key = Convert.FromBase64String(parts[2]);
+
+            using (var hashFunc = new Rfc2898DeriveBytes(
+                password,
+                salt,
+                iterations,
+                HashAlgorithmName.SHA256))
+            {
+                var keyToCheck = hashFunc.GetBytes(_keySize);
+
+                return (keyToCheck.SequenceEqual(key), _options.HashIterations != iterations);
+            }
         }
 
         public string Hash(string password)
